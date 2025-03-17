05ddfd41c1b6708c542e5f8bf491a300db92f5af
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2025-03-17 03:47:07 +0100

    short backend

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const { JWT } = require("google-auth-library");
+
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+
+app.get("/", (req, res) => {
+  console.log("Server is running");
+  res.send("Hello World!");
+});
+
+app.post("/save-to-google-sheets", async (req, res) => {
+  try {
+    // Create a new JWT client
+    const jwtClient = new JWT({
+      email: process.env.CLIENT_EMAIL,
+      key: process.env.PRIVATE_KEY,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
+    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID, jwtClient);
+
+    // Load the spreadsheet
+    // "client_email": "gal-930@erni-ai.iam.gserviceaccount.com",
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0]; // Access first sheet
+
+    // Append new row
+    await sheet.addRow(req.body);
+
+    console.log("Data saved successfully!");
+  } catch (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log(req.body);
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("server error", err);
+  } else {
+    console.log(`check running server on url http://localhost:${PORT}`);
+  }
+});
